004d5bd9eeb5b5a9b3de942181474150aa43599e
qernt <golubev.alexey.04@gmail.com>
2021-07-04 23:32:01 +0600

    Something

---
+++ Jora/Assets/Scripts/Player/Moving.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    Animator animator;
+    
+    private void Awake(){
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W)){
+            transform.position = transform.position + new Vector3(0,1,0);
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            sprite.flipX = true;
+            transform.position = transform.position + new Vector3(-1,0,0);
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            transform.position = transform.position + new Vector3(0,-1,0);
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            sprite.flipX = false;
+            transform.position = transform.position + new Vector3(1,0,0);
+        }
+    }
+}
